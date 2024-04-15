685cb6b4eb320980a872c42459f021aba147d0f6
muhammadali901 <165556611+muhammadali901@users.noreply.github.com>
2024-04-15 16:22:52 +0100

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,69 @@
+let greeting: string;
+greeting = "Hello word!";
+console.log(greeting); // problem 1
+let num1: number = 1;
+let num2: number = 2;
+console.log(`sum of ${num1} and ${num2} is: ${num1 + num2}`);
+num1 - num2;
+num1 * num2;
+num1 / num2;
+let a = 1;
+let b = 2;
+a = a * b; //a=2
+b = a / b; // b=1
+a = a / b; // a=2
+let message: string;
+//message = 5; // compilling error cannot assign number value to string
+console.log(`The remainder of ${num1} & ${num2} is ${num1 % num2}`);
+let counter = 0;
+counter = counter + 1;
+counter += 1;
+counter++;
+let aa = true; let bb = false; let cc = true;
+aa && bb && cc  // AND GATE false
+aa
+!aa;!bb;!cc; // NOTE GATE
+let num = 10;
+num += 1;
+num -= 1;
+num *= 1;
+num /= 1;
+let numX = 12;
+if(numX % 2 == 0){
+       console.log(` ${numX} is a even num. `)
+} 
+else {
+       console.log(` ${numX} is an odd number.
+}
+let age:number;
+let currentYear = 2024;
+let birthYear = 2004;
+age = currentYear - birthYear;
+if(age >= 18){
+     console.log(`Yes. you are eligible for vote`)
+}
+    else{
+        console.log(`Sorry. you are not eligible to vote yet.)
+    } 
+let score:number = 88;
+let grade:string;
+grade = score >= 90 ?  "Your Grade is A":
+                SCORE >= 80 ? "Your grade is B":
+                score >= 70 ? "Your grade is C":
+                SCORE >= 60 ? "Your grade is D": " your grade is F";
+ console.log(grade);
+ let X = 24; let y = 14; let Z = -45;
+ if(X > Y && X > Z){
+    console.log(`$ {X} is the greater number`)
+ }   
+else if(y > x && y > z)
+    console.log(`${y} is the greater number`)
+}
+else{
+    console.log(`${z} is the greater number`)
+}
+   
+
+              
+                
+     
